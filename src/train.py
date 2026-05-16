e0bd9fc7ce32c270b847dfdf1bdae5cee6927976
YannAhlgrim <yann@ahlgrim.net>
2026-05-16 12:47:41 +0200

    import dist

--- src/train.py
+++ src/train.py
@@ -25,6 +25,7 @@ import yaml
 import numpy as np
 
 import torch
+import torch.distributed as dist
 import torch.multiprocessing as mp
 import torch.nn.functional as F
 from torch.nn.parallel import DistributedDataParallel
