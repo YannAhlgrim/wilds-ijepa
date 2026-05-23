d4a418fbd9069c866a2aa875a6c80ec1ac8eb57d
YannAhlgrim <yann@ahlgrim.net>
2026-05-23 13:57:26 +0200

    sys path

--- tools/run_grid.py
+++ tools/run_grid.py
@@ -1,10 +1,15 @@
 import argparse
 import itertools
 import os
+import sys
 import tempfile
 
 import yaml
 import submitit
+
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+if PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_ROOT)
 
 from src.train_supervised import main as app_main
 
